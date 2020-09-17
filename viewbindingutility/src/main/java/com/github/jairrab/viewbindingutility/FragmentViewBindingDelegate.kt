94517c92b224f1f2277a1bd38d5cab5409a1eda7
Jay Barria <jaybarria@gmail.com>
2020-09-17 11:53:16 +0800

    Update FragmentViewBindingDelegate.kt

--- viewbindingutility/src/main/java/com/github/jairrab/viewbindingutility/FragmentViewBindingDelegate.kt
+++ viewbindingutility/src/main/java/com/github/jairrab/viewbindingutility/FragmentViewBindingDelegate.kt
@@ -24,6 +24,7 @@ class FragmentViewBindingDelegate<T : ViewBinding>(
                 fragment.viewLifecycleOwnerLiveData.observe(fragment) { viewLifecycleOwner ->
                     viewLifecycleOwner.lifecycle.addObserver(object : DefaultLifecycleObserver {
                         override fun onDestroy(owner: LifecycleOwner) {
+                            viewLifecycleOwner.lifecycle.removeObserver(this)
                             _binding = null
                         }
                     })
